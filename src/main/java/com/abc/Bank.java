919da9e383040c999b20556a4c21d7faad987fef
KrishnaTejaswi19 <gundimeda.tejaswi@infosys.com>
2024-11-06 20:27:40 +0530

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -35,12 +35,9 @@ public class Bank {
     }
 
     public String getFirstCustomer() {
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
+if (customers.isEmpty()) {
+        return "No customers available";  // Return a meaningful message
     }
+    return customers.get(0).getName(); 
+}
 }
